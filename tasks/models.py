84a5b75291e93e1f5b5e3ec5846163b5ed3948b4
yevpy <eugene2piskun@gmail.com>
2025-04-16 17:07:08 +0300

    Add task app to settings, add models and made migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.content
